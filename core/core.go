084e8e2426b70ed17cc848938d4bc207f6d23928
Shmliy <42091283+hanakoNya@users.noreply.github.com>
2026-03-28 13:09:19 +0800

    Update

--- core/core.go
+++ core/core.go
@@ -20,13 +20,13 @@ import (
 var (
 	Version_x byte = 26
 	Version_y byte = 3
-	Version_z byte = 27
+	Version_z byte = 28
 )
 
 var (
 	build    = "Custom"
 	codename = "Xray, Penetrates Everything."
-	intro    = "A unified platform for anti-censorship."
+	intro    = "A unified platform for anti-censorship.(XICMPv6 Support)"
 )
 
 func init() {
